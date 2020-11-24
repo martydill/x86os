ffb3c99d89e82481a1597c56811d8c138a4e50e4
Marty Dill <martydill@users.noreply.github.com>
2020-11-23 20:04:22 -0800

    Start of a few vfs functions

--- kernel/fs.c
+++ kernel/fs.c
@@ -6,13 +6,27 @@
 
 Filesystem* RootFS = NULL;
 
-FSMount(Device* device, char* mountPoint, BYTE type)
+
+int FSMount(char* deviceName,  char* mountPoint, BYTE type)
 {
+  Device* device = GetDevice(deviceName);
+  if(device == NULL) {
+    return -1;
+  }
+
   Filesystem* fs = (Filesystem*)KMalloc(sizeof(Filesystem));
+  if(fs == NULL) {
+    return -1;
+  }
+
   fs->Type = type;
   fs->Device = device;
-  fs->MountPoint = KMalloc(Strlen(mountPoint));
-  Memcopy(&fs->MountPoint, mountPoint, Strlen(mountPoint));
+  fs->MountPoint = KMalloc(Strlen(mountPoint) + 1);
+  if(fs->MountPoint == NULL) {
+    return -1;
+  }
+
+  Memcopy(fs->MountPoint, mountPoint, Strlen(mountPoint) + 1);
   return fs;
 }
 
@@ -23,7 +37,22 @@ STATUS FSUnmount(char* mountPoint)
 
 STATUS FSInit()
 {
-  Device* floppyDevice = GetDevice("Floppy");
-  RootFS = FSMount(floppyDevice, "/", FS_TYPE_FAT);
+  RootFS = FSMount("floppy0", "/mnt/floppy0", FS_TYPE_FAT);
+  Debug("Found rootfs %u %s\n", RootFS, RootFS->MountPoint);
   return S_OK;
+}
+
+Device* FSDeviceForPath(char* path) {
+  Debug("Searching for path %s %s\n", path, RootFS->MountPoint);
+  if(path == NULL) {
+    return NULL;
+  }
+  Debug("%s %s\n", path, RootFS->MountPoint);
+  if(!Strcmp(path, RootFS->MountPoint)) {
+    Debug("Found match\n");
+    return RootFS->Device;
+  }
+  Debug("No match");
+  // TODO - search VFS tree
+  return NULL;
 }
