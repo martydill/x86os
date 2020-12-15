4c9ed6c2ff788aad35b0c2ce3095d2148fc0a21b
Marty Dill <martydill@users.noreply.github.com>
2020-12-14 20:03:07 -0800

    Quick hack to the shell to send in the correct app name

--- apps/shellx.c
+++ apps/shellx.c
@@ -9,8 +9,17 @@ int main(int argc, char* argv[])
     int bytes = read(0, buf, 255);
     if(bytes > 0) {
       buf[bytes - 1] = 0;
+
+      char binaryName[255];
+      char* p = buf;
+      char* q = binaryName;
+      while(*p != ' ') {
+        *q++ = *p++;
+      }
+      *q = 0;
+
       pid_t pid;
-      posix_spawn(&pid, "hello", 0, 0, buf, 0);
+      posix_spawn(&pid, binaryName, 0, 0, buf, 0);
     }
   }
 }
