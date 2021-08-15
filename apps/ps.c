0f04440fbf7d074d231628bfe657d08b1b85733e
Marty Dill <martydill@users.noreply.github.com>
2021-08-15 09:30:35 -0700

    Super simple ps command

---
+++ apps/ps.c
@@ -0,0 +1,34 @@
+#include "kernel_shared.h"
+
+int main(int argc, char* argv[]) {
+  DIR* dir = opendir("/proc");
+  if(dir == 0) {
+    write(1, "Could not open dir\n", 20);
+    return 1;
+  }
+
+	write(1, "pid\t\tcommand\n", 100);
+  struct dirent *d;
+  do {
+	  char fullPath[255];
+    d = readdir(dir);
+    if(d != NULL) {
+        struct stat statbuf;
+		sprintf(255, fullPath, "/proc/%s", d->d_name);
+        if(stat(fullPath, &statbuf) == 0) {
+          write(1, d->d_name, 20);
+          if(statbuf.st_mode == S_IFDIR) {
+            write(1, "/", 1);
+          }
+          write(1, "\n", 1);
+        }
+        else {
+          write(1, "Stat failed\n", 20);
+        }
+    }
+  }
+  while(d != NULL);
+
+  closedir(dir);
+  return 0;
+}
