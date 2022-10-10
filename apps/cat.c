7fddba42f22a0f39195bdde2f81ee31b2849ab93
Marty Dill <martydill@users.noreply.github.com>
2022-10-09 20:46:59 -0700

    Make cat support multiple files in a single call

--- apps/cat.c
+++ apps/cat.c
@@ -1,22 +1,25 @@
 
 int main(int argc, char* argv[]) {
-  if (argc != 2) {
-    write(1, "Usage: cat <filename>\n", 22);
+  if (argc < 2) {
+    write(1, "Usage: cat filename1 filename2 ...\n", 35);
     return 1;
   }
-
-  int fd = open(argv[1], 0);
-  if (fd == -1) {
-    write(1, "Could not open file\n", 20);
-    return 1;
-  }
-
-  while (1) {
-    char buf[256];
-    int nbytes = read(fd, buf, sizeof(buf));
-    if (nbytes <= 0) {
-      return 0;
+  for (int i = 1; i < argc; ++i) {
+    int fd = open(argv[i], 0);
+    if (fd == -1) {
+      write(1, "Could not open file\n", 20);
+      return 1;
     }
-    write(1, buf, nbytes);
+
+    while (1) {
+      char buf[256];
+      int nbytes = read(fd, buf, sizeof(buf));
+      if (nbytes <= 0) {
+        break;
+      }
+      write(1, buf, nbytes);
+    }
   }
+
+  return 0;
 }
